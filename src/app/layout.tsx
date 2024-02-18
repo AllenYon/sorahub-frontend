4f3e6254f0f5fc9df8fd10c42f0a2013fdeb85c3
alin <>
2024-02-18 11:47:37 -0400

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <h1>Layout</h1>
+      {children}
+      <h3>Layout2</h3>
+      </body>
     </html>
   );
 }
